f35c68ae809ce6b76c83c4d3d77bc7c155feee7a
Andrew B Leland <lel20001@byui.edu>
2022-12-15 11:37:18 -0700

    fixed collision

--- Game1.cpp
+++ Game1.cpp
@@ -4,7 +4,7 @@
 #include <raylib.h>
 
 int WIDTH = 2560;
-int HEIGHT = 1440;
+int HEIGHT = 1240;
 
 int DEATH_ZONE = 5000;
 
@@ -13,8 +13,8 @@ float STARTING_Y = -1000;
 
 typedef struct Player {
 
-	Rectangle pos_size{ STARTING_X,STARTING_Y,50,100 };
-	Rectangle old_pos{ pos_size.x,pos_size.y,pos_size.width,pos_size.height };
+	Rectangle posAndSize{ STARTING_X,STARTING_Y,50,100 };
+	Rectangle old_pos{ posAndSize.x,posAndSize.y,posAndSize.width,posAndSize.height };
 
 	Vector2 camera_coord = { 948,200 };
 	Vector2 camera_vel = { 0,0 };
@@ -36,14 +36,11 @@ typedef struct Player {
 
 	bool isJumping = false;
 	bool isGrounded = false;
-	bool onWallR = false;
-	bool onWallL = false;
 
 } Player;
 
 enum Platform_Type {
-	normal,
-	wall_jump,
+	norm,
 	ice,
 	sand,
 	jumpy,
@@ -76,28 +73,11 @@ class Current_Level {
 
 	public:
 		std::vector<Platforms_Data> platforms = {
-			{100, 700, 1900, 50, ice},
-			{1200, 600, 50, 50, jumpy},
-			{400, 400, 50,50, normal},
-			{900, 600, 50, 100, normal},
-			{650, 500, 50, 50, normal},
-			{650, 300, 50, 50, normal},
-			{400, 200, 50, 50, normal},
-			{650, 100, 50, 50, normal},
-			{900, 0, 50, 50, normal},
-			{650, -100, 50, 50, normal},
-			{0, -200, 400, 50, normal},
-			{-2000, 800, 1900, 50, sand},
-			{-1000, -300, 1000, 50, sand},
-			{2000, 800, 50, 50, normal},
-			{1500, 900, 1400, 100, ice},
-			{2900, 0, 50, 900, wall_jump},
+			{100, 700, 1900, 50, norm},
+			{300, 450, 100, 100, norm}
 		};
 
 		std::vector<Assets_Data> assets = {
-			{100,600,50,50,decor},
-			{300,550,50,50,decor},
-			{1200, 0, 50, 600, decor},
 		};
 
 		//debug colors
@@ -105,30 +85,35 @@ class Current_Level {
 		Color asset_color{ 30,30,30,255 };
 };
 
+bool CollisionCheck(Player * player, Rectangle platform) {
+	if (((platform.x < player->posAndSize.x && player->posAndSize.x < platform.x + platform.width) || 
+		(platform.x < player->posAndSize.x + player->posAndSize.width && player->posAndSize.x + player->posAndSize.width < platform.x + platform.width)) &&
+		((platform.y < player->posAndSize.y && player->posAndSize.y < platform.y + platform.height) ||
+		(platform.y < player->posAndSize.y + player->posAndSize.height && player->posAndSize.y + player->posAndSize.height < platform.y + platform.height)))
+	{
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
 void do_physics(Player* player) {
 
 	float deltaTime = GetFrameTime();
 
-	player->old_pos = player->pos_size;
+	player->old_pos = player->posAndSize;
 
-	if (player->pos_size.y > DEATH_ZONE) {
-		player->pos_size.x = STARTING_X;
-		player->pos_size.y = STARTING_Y;
+	if (player->posAndSize.y > DEATH_ZONE) {
+		player->posAndSize.x = STARTING_X;
+		player->posAndSize.y = STARTING_Y;
 		player->y_vel = 0;
 		player->x_vel = 0;
 	}
 
-	if (player->isGrounded || player->onWallR || player->onWallL) {
+	if (player->isGrounded) {
 		if (IsKeyPressed(KEY_SPACE)) {
 			player->y_vel = player->jump;
-			if (!player->isGrounded) {
-				if (player->onWallR) {
-					player->x_vel = 200;
-				}
-				else if (player->onWallL) {
-					player->x_vel = -200;
-				}
-			}
 		}
 		if (IsKeyDown(KEY_A) && player->x_vel > -player->speed) {
 			player->x_vel -= player->acc * deltaTime;
@@ -222,71 +207,34 @@ void do_physics(Player* player) {
 			}
 		}
 	}
-	player->pos_size.x += player->x_vel * deltaTime;
+	player->posAndSize.x += player->x_vel * deltaTime;
 
 	if (player->y_vel < player->term_vel) {
 		player->y_vel += player->grav * deltaTime;
 	}
 
-	player->pos_size.y += player->y_vel * deltaTime;
+	player->posAndSize.y += player->y_vel * deltaTime;
 }
 
 void collision_detection(Player* player, std::vector<Platforms_Data> platforms) {
 
-	//std::printf("%.2f %.2f\n", player->pos_size.x, player->pos_size.y);
+	//std::printf("%.2f %.2f\n", player->posAndSize.x, player->posAndSize.y);
 	//std::printf("%.2f\n", player->y_vel);
 
-	float deltaTime = 0.01;
-
 	for (int i = 0; i < platforms.size(); i++) {
-		if (CheckCollisionRecs(player->pos_size, Rectangle{ platforms[i].x, platforms[i].y, platforms[i].width, platforms[i].height })) {
+		if (CollisionCheck(player, Rectangle{ platforms[i].x, platforms[i].y, platforms[i].width, platforms[i].height })) {
 			//If my old position is above the platform
-			if (player->old_pos.y + player->old_pos.height < platforms[i].y) {
+			if (player->old_pos.y + player->old_pos.height <= platforms[i].y) {
 				float y_recoil = player->y_vel * 0.85;
 				player->y_vel = 0;
 				player->isGrounded = true;
-				player->pos_size.y = platforms[i].y - player->pos_size.height - deltaTime;
-
-				//Platform type results
-				if (platforms[i].type == ice) {
-					player->acc = 1970;
-					player->decc = 450;
-					player->speed = 800;
-					player->jump = -1000;
-				}
-				else if (platforms[i].type == sand) {
-					player->acc = 1100;
-					player->decc = 5000;
-					player->speed = 300;
-					player->jump = -600;
-				}
-				else if (platforms[i].type == jumpy) {
-					player->y_vel = -y_recoil;
-					//player->jump = -3000;
-					//player->speed = 600;
-					//player->acc = 1970;
-					//player->decc = 1900;
-				}
-				else if (platforms[i].type == normal) { // enum -> normal
-					player->speed = 600;
-
-					player->acc = 1970;
-					player->air_acc = 1200;
-
-					player->decc = 1900;
-					player->air_decc = 700;
-
-					player->grav = 3500;
-					player->term_vel = 10000;
-
-					player->jump = -1000;
-				}
+				player->posAndSize.y = platforms[i].y - player->posAndSize.height;
 				break;
 			}
 			//If my old position is below the platform
-			if (player->old_pos.y > platforms[i].y + platforms[i].height) {
+			if (player->old_pos.y >= platforms[i].y + platforms[i].height) {
 				player->y_vel = 0;
-				player->pos_size.y = platforms[i].y + platforms[i].height + deltaTime;
+				player->posAndSize.y = platforms[i].y + platforms[i].height;
 				break;
 			}
 		}
@@ -295,43 +243,27 @@ void collision_detection(Player* player, std::vector<Platforms_Data> platforms) 
 		}
 	}
 	for (int i = 0; i < platforms.size(); i++) {
-		if (CheckCollisionRecs(player->pos_size, Rectangle{ platforms[i].x, platforms[i].y, platforms[i].width, platforms[i].height })) {
+		if (CollisionCheck(player, Rectangle{ platforms[i].x, platforms[i].y, platforms[i].width, platforms[i].height })) {
 			//If my old position is to the left of the platform
-			if (player->old_pos.x + player->old_pos.width < platforms[i].x) {
+			if (player->old_pos.x + player->old_pos.width <= platforms[i].x) {
 				player->x_vel = 0;
-				player->pos_size.x = platforms[i].x - player->pos_size.width - deltaTime;
-				if (platforms[i].type == wall_jump) {
-					player->onWallL = true;
-					player->onWallR = false;
-					player->term_vel = 200;
-				}
+				player->posAndSize.x = platforms[i].x - player->posAndSize.width;
 				break;
 			}
 			//If my old position is to the right of the platform
-			if (player->old_pos.x > platforms[i].x + platforms[i].width) {
+			if (player->old_pos.x >= platforms[i].x + platforms[i].width) {
 				player->x_vel = 0;
-				player->pos_size.x = platforms[i].x + platforms[i].width + deltaTime;
-				if (platforms[i].type == wall_jump) {
-					player->onWallR = true;
-					player->onWallL = true;
-					player->term_vel = 200;
-				}
+				player->posAndSize.x = platforms[i].x + platforms[i].width;
 				break;
 			}
-		}
-		else {
-			player->grav = 3500;
-			player->term_vel = 10000;
-			player->onWallR = false;
-			player->onWallL = false;
 		}
 	}
 }
 
 void camera_movement(Player * player) {
 	float deltaTime = GetFrameTime();
-	player->camera_vel.x = (6.7 * (player->pos_size.x - player->camera_coord.x));
-	player->camera_vel.y = (6.7 * (player->pos_size.y - player->camera_coord.y));
+	player->camera_vel.x = (6.7 * (player->posAndSize.x - player->camera_coord.x));
+	player->camera_vel.y = (6.7 * (player->posAndSize.y - player->camera_coord.y));
 	player->camera_coord.x += player->camera_vel.x * deltaTime;
 	player->camera_coord.y += player->camera_vel.y * deltaTime;
 }
@@ -364,8 +296,8 @@ void draw_screen(Current_Level level, Player* player, Camera2D camera) {
 		for (int i = 0; i < level.assets.size(); i++) {
 			DrawRectangle(level.assets[i].x, level.assets[i].y, level.assets[i].width, level.assets[i].height, level.asset_color);
 		}
-		DrawRectangleLines(player->pos_size.x, player->pos_size.y, player->pos_size.width, player->pos_size.height, { 255, 0, 0, 255 });
-		//DrawPixel(player->pos_size.x, player->pos_size.y, Color{ 0,255,0,255 });
+		DrawRectangleLines(player->posAndSize.x, player->posAndSize.y, player->posAndSize.width, player->posAndSize.height, { 255, 0, 0, 255 });
+		//DrawPixel(player->posAndSize.x, player->posAndSize.y, Color{ 0,255,0,255 });
 		//DrawPixel(player->camera_coord.x, player->camera_coord.y, Color{ 255,0,0,255 });
 		//
 
@@ -387,7 +319,7 @@ int main(void)
 
 	Camera2D camera;
 	camera.zoom = 3;
-	camera.offset = { (float) (WIDTH / 2) - (player.pos_size.width + 25), (float) (HEIGHT / 2) - (player.pos_size.height + 50)};
+	camera.offset = { (float) (WIDTH / 2) - (player.posAndSize.width + 25), (float) (HEIGHT / 2) - (player.posAndSize.height + 50)};
 	camera.rotation = 0;
 
 	//if fps is > 1000 or < 12, you fall through the platform
---
+++ README.md
@@ -0,0 +1,31 @@
+# Overview
+With this sprint I was trying to acomplish a 2d platformer with working physics.
+
+This software provides a basic game engine for people looking to build a 2d platformer game.
+
+I wrote this software so that I could make a video game and possibly sell it.
+
+[Software Demo Video](https://www.youtube.com/watch?v=XRg_Z8g-CYA)
+
+# Development Environment
+
+I used:
+1. visual studio
+2. my laptop
+
+Language and Libraries:
+1. c++
+2. raylib which is a game library for creating 2d and 3d games!
+
+# Useful Websites
+
+{Make a list of websites that you found helpful in this project}
+* [W3Schools](https://www.w3schools.com/)
+* [youtube](https://www.youtube.com/)
+
+# Future Work
+
+{Make a list of things that you need to fix, improve, and add in the future.}
+* I need to improve my time management
+* I need to fix collision because it doesn't work the greatest
+* I want to add enemies!
